795163bce366edc546d2289152164d53d1eea119
joel <joel@deb.casa.org>
2023-11-06 17:57:57 -0500

    add object oriented programming to pucp bachelor degree

--- bachelor_degree/pucp_bachelor.py
+++ bachelor_degree/pucp_bachelor.py
@@ -4,62 +4,96 @@ from collections import deque
 import requests
 
 
-def get_faculties_page():
-    faculties_req = requests.get("https://repositorio.pucp.edu.pe/index/handle/123456789/7312", verify=False)
-    try:
-        if faculties_req.status_code == 200:
-            faculties_page = BeautifulSoup(faculties_req.text, "html.parser")
-            return faculties_page
-    except requests.exceptions.HTTPError as error:
-        print(f"An ocurred error: {error}")
+class PucpFaculties:
+    def __init__(self, faculties_url):
+        self.faculties_url = faculties_url
 
+    def get_faculties_url(self):
+        return self.faculties_url
 
-def get_faculties(page):
-    faculties = page.find_all("span", class_="Z3988", limit=13)
-    faculties_list = [faculty.string for faculty in faculties]
-    return faculties_list
+    def get_faculties_response(self):
+        faculties_req = requests.get(self.faculties_url, verify=False)
+        return faculties_req
 
-
-def get_thesis_count_pages():
-    thesis_count_page = deque()
-    page_ids = [9117, 170514, 9118, 129392, 135248, 9119, 9120, 9121, 9122,
-                9123, 9124, 9125, 129361]
-
-    for value in page_ids:
-        thesis_request = requests.get(f"https://repositorio.pucp.edu.pe/index/handle/123456789/{value}/browse?type=dateissued", verify=False)
+    def get_faculties_page(self, response):
         try:
-            if thesis_request.status_code == 200:
-                thesis_page = BeautifulSoup(thesis_request.text, "html.parser")
-                thesis_count_page.append(thesis_page)
+            if response.status_code == 200:
+                page = BeautifulSoup(response.text, "html.parser")
+                return page
         except requests.exceptions.HTTPError as error:
             print(f"An ocurred error: {error}")
 
-    return thesis_count_page
+    def get_faculties(self, page):
+        faculties = page.find_all("span", class_="Z3988", limit=13)
+        faculties_list = [faculty.string for faculty in faculties]
+        return faculties_list
 
 
-def get_thesis_count(pages):
-    thesis_list = deque()
+class PucpThesis:
+    # https://repositorio.pucp.edu.pe/index/handle/123456789/{value}/browse?type=dateissued
+    def __init__(self, thesis_url):
+        self.thesis_url = thesis_url
+        self.thesis_ids = [9117, 170514, 9118, 129392, 135248, 9119, 9120, 9121, 9122, 9123, 9124, 9125, 129361]
 
-    for page in pages:
-        thesis_paragraph_list = page.find_all("p", class_="pagination-info")
-        thesis_string_list = [thesis.string for thesis in thesis_paragraph_list]
-        thesis_string = thesis_string_list[0]
-        options = ["Now showing items 1-20 of ",
-                   "Now showing items 1-6 of ",
-                   "Now showing items 1-15 of "]
-        for option in options:
-            if option in thesis_string:
-                thesis_count = thesis_string.replace(option, "")
-                thesis_list.append(int(thesis_count))
+    def get_thesis_url(self):
+        return self.thesis_url
 
-    return thesis_list
+    def get_thesis_ids(self):
+        return self.thesis_ids
+
+    def get_thesis_response(self):
+        thesis_response = deque()
+        for thesis in self.thesis_ids:
+            thesis_req = requests.get(f"{self.thesis_url}/{thesis}/browse?type=dateissued", verify=False)
+            thesis_response.append(thesis_req)
+        return thesis_response
+
+    def get_thesis_page(self, responses):
+        thesis_pages = deque()
+
+        for response in responses:
+            try:
+                if response.status_code == 200:
+                    page = BeautifulSoup(response.text, "html.parser")
+                    thesis_pages.append(page)
+            except requests.exceptions.HTTPError as error:
+                print(f"An ocurred error: {error}")
+        return thesis_pages
+
+    def get_thesis_position(self, pages):
+        thesis_positions_list = deque()
+
+        for page in pages:
+            thesis_p = page.find_all("p", class_="pagination-info")
+            thesis_string = [thesis.string for thesis in thesis_p]
+            thesis_position = thesis_string[0]
+            thesis_positions_list.append(thesis_position)
+        return thesis_positions_list
+
+    def get_thesis_count(self, positions):
+        thesis_count_list = deque()
+
+        for position in positions:
+            options = ["Now showing items 1-20 of ", "Now showing items 1-6 of ", "Now showing items 1-15 of "]
+
+            for option in options:
+                if option in position:
+                    thesis_count = position.replace(option, "")
+                    thesis_count_list.append(thesis_count)
+        return thesis_count_list
 
 
 def get_pucp():
     module = import_module("bachelor_degree.utils.thesis")
 
     print("Getting data from PUCP...")
-    faculties = get_faculties(get_faculties_page())
-    thesis = get_thesis_count(get_thesis_count_pages())
+    faculties_url = PucpFaculties("https://repositorio.pucp.edu.pe/index/handle/123456789/7312")
+    faculties_pages = faculties_url.get_faculties_page(faculties_url.get_faculties_response())
+    faculties_list = faculties_url.get_faculties(faculties_pages)
 
-    module.write_csv(faculties, thesis, "PUCP_BACHELOR.csv")
+    thesis_url = PucpThesis("https://repositorio.pucp.edu.pe/index/handle/123456789/")
+    thesis_pages = thesis_url.get_thesis_page(thesis_url.get_thesis_response())
+    thesis_positions = thesis_url.get_thesis_position(thesis_pages)
+    thesis_count = thesis_url.get_thesis_count(thesis_positions)
+
+    module.write_csv(faculties_list, thesis_count, "PUCP_BACHELOR.csv")
