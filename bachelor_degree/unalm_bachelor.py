52d0e1a04493f85134e0ba34860bca3b0c10757b
joel <joel@deb.casa.org>
2023-11-06 19:03:13 -0500

    add object oriented programming to unalm bachelor degree

--- bachelor_degree/unalm_bachelor.py
+++ bachelor_degree/unalm_bachelor.py
@@ -4,75 +4,194 @@ from collections import deque
 import requests
 
 
-def get_faculties_page():
-    req = requests.get("https://repositorio.lamolina.edu.pe/", verify=False)
-    try:
-        if req.status_code == 200:
-            faculties_page = BeautifulSoup(req.text, "html.parser")
-            return faculties_page
-    except requests.exceptions.HTTPError as error:
-        print(f"An ocurred error: {error}")
+class UnalmFaculties:
+    def __init__(self, faculties_url):
+        self.faculties_url = faculties_url
+
+    def get_faculties_url(self):
+        return self.faculties_url
+
+    def get_faculties_response(self):
+        faculties_req = requests.get(self.faculties_url, verify=False)
+        return faculties_req
+
+    def get_faculties_page(self, response):
+        try:
+            if response.status_code == 200:
+                page = BeautifulSoup(response.text, "html.parser")
+                return page
+        except requests.exceptions.HTTPError as error:
+            print(f"An ocurred error: {error}")
+
+    def get_faculties(self, page):
+        faculties = page.find_all("a", attrs={"name": "community-browser-link"}, limit=8)
+        faculties_list = [faculty.string for faculty in faculties]
+        return faculties_list
 
 
-def get_faculties(page):
-    faculties = page.find_all("a", attrs={"name": "community-browser-link"}, limit=8)
-    faculties_list = [faculty.string for faculty in faculties]
-    return faculties_list
+class UnalmThesis:
+    def __init__(self, thesis_url):
+        self.thesis_url = thesis_url
+        self.thesis_ids = [[12, 13, 14, 15, 16],
+                           [17, 18, 19, 20],
+                           [21, 22],
+                           [23, 25, 26, 24],
+                           [27, 28],
+                           [29, 30, 31],
+                           [32, 33],
+                           [192, 34]]
 
+    def get_thesis_url(self):
+        return self.thesis_url
 
-def get_thesis_count_page():
-    thesis_count_pages = []
-    thesis_ids = [[12, 13, 14, 15, 16],
-                  [17, 18, 19, 20],
-                  [21, 22],
-                  [23, 25, 26, 24],
-                  [27, 28],
-                  [29, 30, 31],
-                  [32, 33],
-                  [192, 34]]
+    def get_thesis_response(self):
+        thesis_responses = deque()
 
-    for i in range(len(thesis_ids)):
-        thesis_recovered_page = []
-        for id in thesis_ids[i]:
-            req = requests.get(f"https://repositorio.lamolina.edu.pe/handle/20.500.12996/{id}", verify=False)
-            try:
-                if req.status_code == 200:
-                    thesis_page = BeautifulSoup(req.text, "html.parser")
-                    thesis_recovered_page.append(thesis_page)
-            except requests.exceptions.HTTPError as error:
-                print(f"An ocurred error: {error}")
+        for i in range(len(self.thesis_ids)):
+            recovered_responses = deque()
 
-        thesis_count_pages.append(thesis_recovered_page)
+            for thesis_id in self.thesis_ids[i]:
+                req = requests.get(f"{self.thesis_url}/{thesis_id}", verify=False)
+                recovered_responses.append(req)
 
-    return thesis_count_pages
+            thesis_responses.append(recovered_responses)
+        return thesis_responses
 
+    def get_thesis_page(self, responses):
+        thesis_pages = deque()
 
-def get_thesis_count(pages):
-    thesis_list = deque()
+        for i in range(len(responses)):
+            recovered_pages = deque()
 
-    for i in range(8):
-        thesis_recovered = deque()
+            for response in responses[i]:
+                try:
+                    if response.status_code == 200:
+                        page = BeautifulSoup(response.text, "html.parser")
+                        recovered_pages.append(page)
+                except requests.exceptions.HTTPError as error:
+                    print(f"An ocurred error: {error}")
 
-        for page in pages[i]:
-            thesis = page.find_all("h4", class_="artifact-title", limit=5)
-            thesis_count = [t.contents[2] for t in thesis]
+            thesis_pages.append(recovered_pages)
+        return thesis_pages
 
-            remove_spaces = thesis_count[-1].strip()
-            remove_brackets = remove_spaces.translate({ord("["): None, ord("]"): None})
-            convert_to_int = int(remove_brackets)
-            thesis_recovered.append(convert_to_int)
+    def get_thesis_position(self, pages):
+        thesis_positions = deque()
 
-        thesis_sum = sum(thesis_recovered)
-        thesis_list.append(thesis_sum)
+        for i in range(len(pages)):
+            recovered_positions = deque()
 
-    return thesis_list
+            for page in pages[i]:
+                thesis = page.find_all("h4", class_="artifact-title", limit=5)
+                thesis_content = [t.contents[2] for t in thesis]
+                recovered_positions.append(thesis_content)
+
+            thesis_positions.append(recovered_positions)
+        return thesis_positions
+
+    def get_thesis_count(self, positions):
+        thesis_count = deque()
+
+        for i in range(len(positions)):
+            recovered_count = deque()
+
+            for position in positions[i]:
+                remove_spaces = position[-1].strip()
+                remove_brackets = remove_spaces.translate({ord("["): None, ord("]"): None})
+                convert_to_int = int(remove_brackets)
+                recovered_count.append(convert_to_int)
+
+            count_added = sum(recovered_count)
+            thesis_count.append(count_added)
+
+        return thesis_count
 
 
 def get_unalm():
     module = import_module("bachelor_degree.utils.thesis")
 
     print("Getting data from UNALM...")
-    faculties = get_faculties(get_faculties_page())
-    thesis = get_thesis_count(get_thesis_count_page())
+    faculties_url = UnalmFaculties("https://repositorio.lamolina.edu.pe/")
+    falcuties_page = faculties_url.get_faculties_page(faculties_url.get_faculties_response())
+    faculties_list = faculties_url.get_faculties(falcuties_page)
 
-    module.write_csv(faculties, thesis, "UNALM_BACHELOR.csv")
+    thesis_url = UnalmThesis("https://repositorio.lamolina.edu.pe/handle/20.500.12996")
+    thesis_pages = thesis_url.get_thesis_page(thesis_url.get_thesis_response())
+    thesis_positions = thesis_url.get_thesis_position(thesis_pages)
+    thesis_count = thesis_url.get_thesis_count(thesis_positions)
+
+    module.write_csv(faculties_list, thesis_count, "UNALM_BACHELOR.csv")
+
+
+######################################
+# def get_faculties_page():
+#     req = requests.get("https://repositorio.lamolina.edu.pe/", verify=False)
+#     try:
+#         if req.status_code == 200:
+#             faculties_page = BeautifulSoup(req.text, "html.parser")
+#             return faculties_page
+#     except requests.exceptions.HTTPError as error:
+#         print(f"An ocurred error: {error}")
+#
+#
+# def get_faculties(page):
+#     faculties = page.find_all("a", attrs={"name": "community-browser-link"}, limit=8)
+#     faculties_list = [faculty.string for faculty in faculties]
+#     return faculties_list
+#
+#
+# def get_thesis_count_page():
+#     thesis_count_pages = []
+#     thesis_ids = [[12, 13, 14, 15, 16],
+#                   [17, 18, 19, 20],
+#                   [21, 22],
+#                   [23, 25, 26, 24],
+#                   [27, 28],
+#                   [29, 30, 31],
+#                   [32, 33],
+#                   [192, 34]]
+#
+#     for i in range(len(thesis_ids)):
+#         thesis_recovered_page = []
+#         for id in thesis_ids[i]:
+#             req = requests.get(f"https://repositorio.lamolina.edu.pe/handle/20.500.12996/{id}", verify=False)
+#             try:
+#                 if req.status_code == 200:
+#                     thesis_page = BeautifulSoup(req.text, "html.parser")
+#                     thesis_recovered_page.append(thesis_page)
+#             except requests.exceptions.HTTPError as error:
+#                 print(f"An ocurred error: {error}")
+#
+#         thesis_count_pages.append(thesis_recovered_page)
+#
+#     return thesis_count_pages
+#
+#
+# def get_thesis_count(pages):
+#     thesis_list = deque()
+#
+#     for i in range(8):
+#         thesis_recovered = deque()
+#
+#         for page in pages[i]:
+#             thesis = page.find_all("h4", class_="artifact-title", limit=5)
+#             thesis_count = [t.contents[2] for t in thesis]
+#
+#             remove_spaces = thesis_count[-1].strip()
+#             remove_brackets = remove_spaces.translate({ord("["): None, ord("]"): None})
+#             convert_to_int = int(remove_brackets)
+#             thesis_recovered.append(convert_to_int)
+#
+#         thesis_sum = sum(thesis_recovered)
+#         thesis_list.append(thesis_sum)
+#
+#     return thesis_list
+#
+#
+# def get_unalm():
+#     module = import_module("bachelor_degree.utils.thesis")
+#
+#     print("Getting data from UNALM...")
+#     faculties = get_faculties(get_faculties_page())
+#     thesis = get_thesis_count(get_thesis_count_page())
+#
+#     module.write_csv(faculties, thesis, "UNALM_BACHELOR.csv")
