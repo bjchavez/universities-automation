97ca24357002f88d52e674db3e3cbb7c92828d71
joel <joel@deb.casa.org>
2023-11-09 12:28:01 -0500

    add object oriented programming to unsch bachelor degree

--- bachelor_degree/unsch_bachelor.py
+++ bachelor_degree/unsch_bachelor.py
@@ -4,34 +4,45 @@ import requests
 import re
 
 
-def get_page():
-    req = requests.get("http://repositorio.unsch.edu.pe/", verify=False)
-    try:
-        if req.status_code == 200:
-            page = BeautifulSoup(req.text, "html.parser")
-            page_find = page.find_all(class_=re.compile("heading"))
-            return page_find
-    except requests.exceptions.HTTPError as error:
-        print(f"An ocurred error: {error}")
+class Unsch:
+    def __init__(self, url):
+        self.url = url
 
+    def get_url(self):
+        return self.url
 
-def get_faculties(page):
-    faculties_content = [faculty.contents[0].string for faculty in page]
-    faculties_list = faculties_content[1:10]
-    return faculties_list
+    def get_response(self):
+        req = requests.get(self.url, verify=False)
+        return req
 
+    def get_page(self, response):
+        try:
+            if response.status_code == 200:
+                page = BeautifulSoup(response.text, "html.parser")
+                page_find = page.find_all(class_=re.compile("heading"))
+                return page_find
+        except requests.exceptions.HTTPError as error:
+            print(f"An ocurred error: {error}")
 
-def get_thesis_count(page):
-    thesis_content = [thesis.contents[2].string for thesis in page]
-    thesis_list = thesis_content[1:10]
-    return thesis_list
+    def get_faculties(self, page):
+        faculties_content = [faculty.contents[0].string for faculty in page]
+        faculties_list = faculties_content[1:10]
+        return faculties_list
+
+    def get_thesis_count(self, page):
+        thesis_content = [thesis.contents[2].string for thesis in page]
+        thesis_list = thesis_content[1:10]
+        return thesis_list
 
 
 def get_unsch():
     module = import_module("bachelor_degree.utils.thesis")
 
     print("Getting data from UNSCH...")
-    faculties = get_faculties(get_page())
-    thesis = get_thesis_count(get_page())
+    unsch = Unsch("http://repositorio.unsch.edu.pe/")
+    page = unsch.get_page(unsch.get_response())
+    faculties_list = unsch.get_faculties(page)
 
-    module.write_csv(faculties, thesis, "UNSCH_BACHELOR.csv")
+    thesis_count = unsch.get_thesis_count(page)
+
+    module.write_csv(faculties_list, thesis_count, "UNSCH_BACHELOR.csv")
