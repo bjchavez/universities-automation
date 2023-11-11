f189dc88b16a635c03d830aed0da52a02859a688
joel <joel@deb.casa.org>
2023-11-11 15:13:45 -0500

    add docstring to the method of write csv

--- bachelor_degree/utils/thesis.py
+++ bachelor_degree/utils/thesis.py
@@ -3,6 +3,18 @@ import csv
 
 
 def write_csv(faculties: list | deque, thesis: list | deque, csv_name: str):
+    """
+    This method will be imported. It works to write to a csv file with the data from universities
+    (numbers of faculties and theses).
+
+    Args:
+        faculties[list | deque]: A list unknown of faculties.
+        thesis[list | deque]: A list unnknown of theses.
+        csv_name[string]: It will be the name of csv file.
+
+    Return:
+        A csv file writed with the data from universities.
+    """
     print("Writing csv file...")
     faculties_thesis_list = list(zip(faculties, thesis))
     try:
