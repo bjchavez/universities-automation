3bcd746f4289556eb790bde8d7c952f43cf625a6
joel <joel@deb.casa.org>
2023-11-12 12:29:18 -0500

    add docstring at main class and their methods

--- bachelor_degree/unjfsc_bachelor.py
+++ bachelor_degree/unjfsc_bachelor.py
@@ -5,17 +5,43 @@ import re
 
 
 class Unjfsc:
+    """
+    This class provides the methods to get faculties and theses data from UNJFSC.
+    """
     def __init__(self, url):
+        """
+        Args:
+            url[string]: University URL.
+        """
         self.url = url
 
     def get_url(self):
+        """
+        Returns:
+            The URL of the university.
+        """
         return self.url
 
     def get_response(self):
+        """
+        This method make http request at university URL.
+
+        Returns:
+            One http response code
+        """
         req = requests.get(self.url, verify=False)
         return req
 
     def get_page(self, response):
+        """
+        This method verfify if the status code of the parameter response is 200.
+
+        Args:
+            response[http]: Http response code.
+
+        Returns:
+            A BeautifulSoup parsed page.
+        """
         try:
             if response.status_code == 200:
                 page = BeautifulSoup(response.text, "html.parser")
@@ -24,17 +50,44 @@ class Unjfsc:
             print(f"An ocurred error: {error}")
 
     def get_faculties(self, page):
+        """
+        This method find all 'a' HTML elements, convert them to strings and remove all commas.
+
+        Args:
+            page[beautifulsoup]: Beautifulsoup parsed page.
+
+        Returns:
+            A list of strings with the faculties names.
+        """
         faculties = page.find_all("a", attrs={"name": "community-browser-link"})
         faculties_string = [faculty.string for faculty in faculties]
         faculties_comma = [f.translate({ord(","): None}) for f in faculties_string]
         return faculties_comma[2:15]
 
     def get_thesis_position(self, page):
+        """
+        This method find all 'mode' CSS classes, locate the lasts and convert them to strings.
+
+        Args:
+            page[beautifulsoup]: Beautifulsoup parsed page.
+
+        Returns:
+            A list with the positions of the theses.
+        """
         thesis = page.find_all(class_=re.compile("mode"))
         thesis_content = [t.contents[2].string for t in thesis]
         return thesis_content[2:15]
 
     def get_thesis_count(self, positions):
+        """
+        This method loops over the positions, removes all blank spaces and brackets, and converts them to integers.
+
+        Args:
+            positions[list]: Positions of the theses.
+
+        Returns:
+            A list of integers with the numbers of each thesis.
+        """
         thesis_count_list = []
 
         for position in positions:
@@ -46,6 +99,9 @@ class Unjfsc:
 
 
 def get_unjfsc():
+    """
+    This method is used to initialize the UNJFSC class objects and write the CSV file.
+    """
     module = import_module("bachelor_degree.utils.thesis")
 
     print("Getting data from UNJFSC...")
